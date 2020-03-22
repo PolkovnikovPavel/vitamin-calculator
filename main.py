7adc81e648ce8d90ddffd4e11bb5d4866af2d1b7
Павел <pavelpolkovnikov334@gmail.com>
2020-03-22 13:00:15 +0700

    first start

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main():
     # api.add_resource(users_resource.DBResource, '/api/v2/db')
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.route("/")
