0b22b41c572035055c1d0b02669cd5397706838a
Павел <pavelpolkovnikov334@gmail.com>
2020-03-23 22:38:58 +0700

    host fixed

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main():
     api.add_resource(users_resource.DBResource, '/api/db')
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.route("/")
